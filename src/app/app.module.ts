620721fc054d49c2ec5576359eb68c88b19faa8a
Piotr Kulpa <p.kulpa@onet.eu>
2018-07-14 13:54:28 +0200

    add components & service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { TablesComponent } from './tables/tables.component';
 import { ContactComponent } from './contact/contact.component';
+
+const appRoutes: Routes = [
+  {path: '', component: CalculatorComponent},
+  {path: 'tabelki', component: TablesComponent},
+  {path: 'kontakt', component: ContactComponent}
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +21,8 @@ import { ContactComponent } from './contact/contact.component';
     ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
