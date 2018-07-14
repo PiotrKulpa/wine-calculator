8b2329d2ca37f53994172c981e98aefd5b32913b
Piotr Kulpa <p.kulpa@onet.eu>
2018-07-14 11:52:29 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { TablesComponent } from './tables/tables.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent,
+    TablesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
