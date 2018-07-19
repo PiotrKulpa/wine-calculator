ce15cc925c0ddf4b17129d5b42ee5fe190fe82ae
Piotr Kulpa <p.kulpa@onet.eu>
2018-07-19 12:09:23 +0200

    add select html and method

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FruitsService } from '../fruits.service'
+import { FruitsService } from '../fruits.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
@@ -12,6 +13,7 @@ export class CalculatorComponent implements OnInit {
   constructor(private fruitsService: FruitsService) { }
 
   fruit: any = [];
+  id: number;
   acid: number;
   suggar: number;
 
@@ -21,8 +23,15 @@ export class CalculatorComponent implements OnInit {
   }
 
   //TODO napisz input select automatycznie, w zaleznosci od wyboru ustaw zawartosc kwasu i cukru w owocach
-  calcIngredients() {
+  onSelect(e) {
+    this.id = e.target.value;
+    this.acid = this.fruit[this.id].acid;
+    this.suggar = this.fruit[this.id].suggar;
+    console.log(this.acid, this.suggar)
+  }
 
+  calcIngredients(form: NgForm) {
+    console.log(form.value)//dostep po nazwie name
   }
 
 }
