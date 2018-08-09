d96417936b0169f7b0174ff577360fa199540713
PiotrKulpa <p.kulpa@onet.eu>
2018-08-09 08:53:14 +0200

    add firestore

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFireStorage } from 'angularfire2/storage';
 import { Observable } from 'rxjs';
 
 
@@ -11,12 +12,25 @@ import { Observable } from 'rxjs';
 })
 export class GalleryComponent implements OnInit {
   items: Observable<any[]>;
-  constructor(db: AngularFirestore) {
-    this.items = db.collection('items').valueChanges();
+  profileUrl: Observable<string | null>;
+  constructor(db: AngularFirestore, private storage: AngularFireStorage) {
+    this.items = db.collection('images').valueChanges();
+    const ref = this.storage.ref('images/');
+    this.profileUrl = ref.getDownloadURL();
+    console.log(this.profileUrl);
   }
 
   ngOnInit() {
-    console.log(this.items)
   }
+
+  addImage(event) {
+  const file = event.target.files[0];
+  console.log(file);
+  //folder/filename.jpg
+   const filePath = `/images/${file.name}`;
+   const ref = this.storage.ref(filePath);
+   const task = ref.put(file);
+  }
+  //TODO jeśli ok(uzyj obietnicy sledzacej postep), to this.profileUrl = ref.getDownloadURL() i zapisujemy dane w bazie
 
 }
